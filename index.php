c950e09af92c5b766b4706d02eac9cc708ab7968
Vexati-art <55270612+Vexati-art@users.noreply.github.com>
2020-10-24 18:41:47 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,222 @@
+<svg width="800" height="600" xmlns="http://www.w3.org/2000/svg">
+ <!-- Created with Method Draw - http://github.com/duopixel/Method-Draw/ -->
+ <g>
+  <title>background</title>
+  <rect fill="#fff" id="canvas_background" height="780" width="900" y="-1" x="-1"/>
+  <g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
+   <rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>
+  </g>
+ </g>
+ <g>
+  <title>Layer 1</title>
+  <path stroke="" id="svg_1" d="m79.88574,471.24693c-20.93255,-21.44313 -60.18361,1.79468 -64.9168,-37.3467c-9.11076,-34.78192 18.7219,-61.95571 37.24261,-83.01673c32.4016,-19.63818 69.76268,-22.01287 104.80325,-31.0118c41.98991,-7.41346 85.70851,-13.74971 121.90576,-43.17629c41.22795,-31.37791 87.14574,-57.49149 136.81417,-57.02884c68.43479,-2.88535 139.23978,-0.08993 202.51193,35.45709c39.77206,21.83569 82.50324,32.9334 124.94924,43.6151c36.11516,5.77439 27.7164,53.15886 39.72855,82.37123c9.16964,33.54486 -10.65274,66.93462 -39.66765,68.0012c-30.67801,8.48297 -66.58195,9.3179 -87.99792,41.89303c-21.9379,29.54428 -61.33043,28.52442 -83.67056,0.30061c-9.23629,-6.97669 -15.4027,-23.868 -27.70206,-20.95233c-114.68646,0.75596 -229.43205,-1.99211 -344.06843,2.4842c-29.95529,-2.90125 -42.39757,42.32202 -74.26102,39.17866c-16.31456,0.61571 -31.76419,-31.43484 -45.67109,-40.76848l0,0.00003z" stroke-width="1.5" fill="#fff"/>
+  <path id="svg_3" d="m55,379l0,73" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_4" d="m74,361c0,0 56,140 56,140" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_5" d="m723,304c0,-1 30,131 30,131" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_6" d="m612,501c0,0 88,-153 88,-153" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_7" d="m668,456c0,1 50,-91 50,-91" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_8" d="m192,450l241,-4" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_9" d="m323,425l261,0" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_10" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_11" d="m122,351l205,-6" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_12" d="m424,298l4,105" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_13" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_14" d="m125,406l267,-8" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_15" d="m329,263l256,-5" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_16" d="m446,293c0,0 195,-3 195,-3" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_17" d="m138,438c0,0 152,-4 152,-5" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_18" d="m115,381l294,-10" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_20" d="m257,325" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_21" d="m241,322l157,-3" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_22" d="m286,289l143,-4" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_23" d="m476,450c0,0 141,-1 141,-1" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_24" d="m459,353c4,0 216,-2 216,-2" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_25" d="m470,391l183,-4" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_26" d="m453,329l245,-5" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_27" d="m70,409l33,70" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_28" d="m30,395c0,0 1,50 1,50" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+  <path id="svg_29" d="m603,434c0,0 33,-30 33,-30" opacity="0.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="" fill="#fff"/>
+ </g>
+
+
+
+  <text y="40" font-size="29.4" fill="red">
+    <textPath href="#svg_1">
+      "I don't run a car, have never run a car. I could say that this is because I have this extremely tender environmentalist conscience, but the fact is I hate driving."
+    </textPath>
+  </text>
+
+    <text x="-3" font-size="22" fill="blue">
+    <textPath href="#svg_3">
+      5-Speed
+
+    </textPath>
+  </text>  
+
+  <text x="-1" font-size="33" fill="green">
+    <textPath href="#svg_4">
+      
+     Engineered
+    </textPath>
+  </text>  
+
+  <text x="-1" font-size="33" fill="#dba502">
+    <textPath href="#svg_5">
+      
+     Expensive
+    </textPath>
+  </text>  
+
+  <text x="-1" font-size="33" fill="#2c26e0">
+    <textPath href="#svg_6">
+      
+     Economical
+    </textPath>
+  </text>  
+
+  <text x="1" font-size="35" fill="#3cc78f">
+    <textPath href="#svg_7">
+      
+     Classic
+    </textPath>
+  </text>  
+
+  <text x="5" font-size="30" fill="#700c11">
+    <textPath href="#svg_8">
+      
+     Custom Designed
+    </textPath>
+  </text>  
+
+  <text x="7" font-size="32" fill="#0f521d">
+    <textPath href="#svg_9">
+      
+     Beyond Compare
+    </textPath>
+  </text>
+
+
+ <text x="7" font-size="34" fill="black">
+    <textPath href="#svg_11">
+      
+     Innovative
+    </textPath>
+  </text>
+
+   <text x="-4" font-size="30" fill="#f0430e">
+    <textPath href="#svg_12">
+      
+     Powerful
+    </textPath>
+  </text>
+
+<text x="3" font-size="33" fill="#95ab0a">
+    <textPath href="#svg_14">
+      
+     Front Wheel Drive 
+    </textPath>
+  </text>
+
+<text x="15" font-size="32" fill="#466675">
+    <textPath href="#svg_15">
+      
+   Family Friendly
+    </textPath>
+  </text>
+
+  <text x="10" font-size="36" fill="#ed0c39">
+    <textPath href="#svg_16">
+      
+   Progressive
+    </textPath>
+  </text>
+
+    <text x="10" font-size="36" fill="#806729">
+    <textPath href="#svg_17">
+      
+   Luxury
+    </textPath>
+  </text>
+
+  <text x="10" font-size="36" fill="#296b38">
+    <textPath href="#svg_18">
+      
+  Environmentally
+    </textPath>
+  </text>
+
+   <text x="10" font-size="40" fill="#6924ad">
+    <textPath href="#svg_21">
+      
+  Worthy
+    </textPath>
+  </text>
+
+<text x="10" font-size="35" fill="#80112a">
+    <textPath href="#svg_22">
+      
+  Hybrid
+    </textPath>
+  </text>
+
+<text x="10" font-size="35" fill="#ed585d">
+    <textPath href="#svg_23">
+      
+  Dynamic
+    </textPath>
+  </text>
+
+<text x="10" font-size="35" fill="#ab1d0e">
+    <textPath href="#svg_24">
+      
+  Easy To Drive
+    </textPath>
+  </text>
+
+  <text x="1" font-size="43" fill="#7491e3">
+    <textPath href="#svg_25">
+      
+  Distinctive
+    </textPath>
+  </text>
+
+   <text x="25" font-size="37" fill="#803c66">
+    <textPath href="#svg_26">
+      
+  High Agility
+    </textPath>
+  </text>
+
+   <text x="1" font-size="30" fill="#374009">
+    <textPath href="#svg_27">
+      
+  Agile
+    </textPath>
+  </text>
+
+   <text x="1" font-size="29" fill="#78242e">
+    <textPath href="#svg_28">
+      
+  Fast
+    </textPath>
+  </text>
+
+   <text x="1" font-size="26" fill="#1e332e">
+    <textPath href="#svg_29">
+      
+  Safe
+    </textPath>
+  </text>
+
+
+
+
+
+
+
+
+
+     
+ </g>
+</svg>
+  
